c79d5ffe9e8d85ebf62a62d43331dd87b72c9b98
Robert Petryszak <rpetryszak@btinternet.com>
2020-05-29 17:15:32 +0100

    For each user-story-*/*-with-output.ipynb notebook, this script generates two files: 1. 'no output jupyter notebook' and 2. 'with output html'

---
+++ render_notebooks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# For each user-story-*/*-with-output.ipynb notebook, this script generates two files: 1. 'no output jupyter notebook' and 2. 'with output html'
+for jn in $(ls user-story-*/*-with-output.ipynb); do
+    no_output_jn=`echo $jn | perl -p -e 's|\-with\-output||'`
+    echo "About to convert $jn to $no_output_jn"
+    jupyter nbconvert --ClearOutputPreprocessor.enabled=True --to notebook --stdout $jn > $no_output_jn
+    html_jn=`echo $jn | perl -p -e 's|\.ipynb|.html|'`
+    echo "About to convert $jn to $html_jn"
+    jupyter nbconvert --to html --stdout $jn > $html_jn
+done
